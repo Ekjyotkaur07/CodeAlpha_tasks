19613c214a1ff883336743abe2ddd50f327ec83d
Ekjyotkaur07 <ekjyotkaurmalhotra@gmail.com>
2026-06-02 02:00:14 +0530

    feat: add main menu with options

--- main.cpp
+++ main.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main() {
-    cout << "To-Do List App Started!" << endl;
+    int choice;
+    
+    do {
+        cout << "\n=== TO-DO LIST MENU ===" << endl;
+        cout << "1. Add Task" << endl;
+        cout << "2. View Tasks" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+        
+        if(choice == 1) {
+            cout << "Add task feature coming soon!" << endl;
+        }
+        else if(choice == 2) {
+            cout << "View tasks feature coming soon!" << endl;
+        }
+        
+    } while(choice != 3);
+    
     return 0;
 }
